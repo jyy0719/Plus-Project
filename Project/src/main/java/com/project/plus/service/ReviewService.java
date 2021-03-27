2ede4b033aa049438bffae44c2598609c4a6aacc
Jinkyung <jk940816@gmail.com>
2021-03-27 12:52:59 +0900

    club, review 수정

--- Project/src/main/java/com/project/plus/service/ReviewService.java
+++ Project/src/main/java/com/project/plus/service/ReviewService.java
@@ -12,14 +12,10 @@ public interface ReviewService {
 	String reviewList();
 	public List<ReviewVO> getReviews(int clubNum);
 	public List<ReviewVO> getMoreReviews(Map<String,Integer> map);
-<<<<<<< HEAD
-	public int getReviewCount();
+	public int getReviewCount(int clubNum);
 	
 	//정연 하단 추가 20210326
 	ReviewVO readReview(ReviewVO vo);
 	void updateReview(ReviewVO vo);
 	void deleteReview(ReviewVO vo);
-=======
-	public int getReviewCount(int clubNum);
->>>>>>> cc7b2e2ad58d9bf5737df9772dbfd322bad86475
 }
